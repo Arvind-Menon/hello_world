6ea66265bdea203147c6accda8d9017da0416f07
Arvind Menon <arvind.menon@virginmediao2.co.uk>
2023-07-10 13:17:24 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from utils import hello_world
+
+result = hello_world("John")
+print(result)
---
+++ utils.py
@@ -0,0 +1,2 @@
+def hello_world(name):
+    return f"With great powers comes great responsibility, {name}. \nWelcome to the world of magic, and use your powers wisely."
